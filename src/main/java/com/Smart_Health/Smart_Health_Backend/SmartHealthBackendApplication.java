5161ff0530420cbcadde9d0b775b75d212112d62
Wah-Ishan <wahasinduishan@gmail.com>
2025-06-18 13:56:28 +0530

    Initial commit

---
+++ src/main/java/com/Smart_Health/Smart_Health_Backend/SmartHealthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Smart_Health.Smart_Health_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHealthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHealthBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Smart_Health/Smart_Health_Backend/SmartHealthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Smart_Health.Smart_Health_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHealthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
