c62ead89ca6f324bc406d4e11c40765fc7b81b58
Wah-Ishan <wahasinduishan@gmail.com>
2025-07-08 21:08:16 +0530

    all done✅

--- src/main/java/com/Smart_Health/Smart_Health_Backend/Report_microService/Repository/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Smart_Health.Smart_Health_Backend.Report_microService.Repository;
-
-import com.Smart_Health.Smart_Health_Backend.Report_microService.Entity.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, Long> {
-}
